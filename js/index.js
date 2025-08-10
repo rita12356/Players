7558b2d10b9d9ee190c9748957d65e00d8214716
rita12356 <wadeew2016@yandex.ru>
2025-08-10 18:23:42 +0500

    initial commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+let item1 = document.querySelector(`#item1`);
+let item2 = document.querySelector(`#item2`);
+let item3 = document.querySelector(`#item3`);
+
+function onCounterClick(evt) {
+    let item = evt.target;
+    let number = item.innerHTML;
+    number = Number(number);
+    number++;
+    item.innerHTML = number;
+}
+item1.addEventListener(`click`, onCounterClick);
+item2.addEventListener(`click`, onCounterClick);
+item3.addEventListener(`click`, onCounterClick);
